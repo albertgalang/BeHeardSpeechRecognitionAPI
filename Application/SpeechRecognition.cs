0c6878c38f8988a9dde17d12a437a556e0f7c779
albertgalang <albrtglng@gmail.com>
2022-02-21 00:39:43 -0800

    Initial commit; Vosk API implementation; Web API post endpoint

---
+++ Application/SpeechRecognition.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using Vosk;
+
+namespace BeHeardSpeechRecognitionAPI.Application
+{
+    public class SpeechRecognition
+    {
+        public static string Predict(string base64)
+        {
+            Vosk.Vosk.SetLogLevel(-1);
+
+            // TODO: Starting the model at entry point asynchronously may improve speed
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var srModel = System.IO.Path.Combine(currentDirectory, @"SRModel");
+            var modelPath = Path.GetFullPath(srModel);
+            var model = new Model(modelPath);
+            VoskRecognizer rec = new VoskRecognizer(model, 16000.0f);
+            rec.SetMaxAlternatives(0);
+            rec.SetWords(true);
+
+            byte[] speechData = Convert.FromBase64String(base64);
+            using (MemoryStream ms = new MemoryStream(speechData))
+            {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = ms.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    if (rec.AcceptWaveform(buffer, bytesRead))
+                    {
+                        var result = rec.Result();
+                    }
+                    else
+                    {
+                        var result = rec.PartialResult();
+                    }
+                }
+            }
+            var prediction = rec.FinalResult();
+            return prediction;
+        }
+    }
+}
---
+++ Controllers/SpeechController.cs
@@ -0,0 +1,18 @@
+﻿using BeHeardSpeechRecognitionAPI.Application;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeHeardSpeechRecognitionAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpeechController : ControllerBase
+    {
+        // POST api/<SpeechController>
+        [HttpPost]
+        public string Post([FromBody] string value)
+        {
+            var result = SpeechRecognition.Predict(value);
+            return result; // should be a json string
+        }
+    }
+}
